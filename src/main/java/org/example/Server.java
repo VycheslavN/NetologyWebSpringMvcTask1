912cad81b38afa11b157019a96ce561c27a0dcf5
Vyacheslav Nechepurenko <masterdog8@mail.ru>
2023-03-30 20:53:07 +0300

    add .png & .svg

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -14,7 +14,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-    final List<String> validPaths = List.of("/index.html", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
     ExecutorService executorService;
 
     public Server(int poolSize) {
